f652838c53961a621cc4aadbf7ad76a3f792e5af
wb_zhangjing47 <wb_zhangjing47@meituan.com>
2025-01-13 17:55:27 +0800

    feat: storage init

---
+++ background/messages/storage.ts
@@ -0,0 +1,16 @@
+import type { PlasmoMessaging } from "@plasmohq/messaging"
+import { Storage } from "@plasmohq/storage"
+
+export const storage = new Storage()
+
+const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
+  if (req.body && req.body.callbackName === "setStorage") {
+    await storage.set(req.body.key, req.body.value)
+  }
+  if (req.body && req.body.callbackName === "getStorage") {
+    const value = await storage.get(req.body.key)
+    res.send({ message: value })
+  }
+}
+
+export default handler
---
+++ contents/storage_relay.ts
@@ -0,0 +1,13 @@
+import type { PlasmoCSConfig } from "plasmo"
+
+import { relayMessage, sendToBackground } from "@plasmohq/messaging"
+import { relay } from "@plasmohq/messaging/relay"
+
+export const config: PlasmoCSConfig = {
+  matches: ["<all_urls>"],
+  run_at: "document_start"
+}
+
+relayMessage({
+  name: "storage"
+})
