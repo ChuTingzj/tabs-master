a09c9e5807e7fd2bb4a898298742af89a5d4a8ef
wb_zhangjing47 <wb_zhangjing47@meituan.com>
2025-01-17 13:49:31 +0800

    fix: 修复根元素字体大小

--- theme.tsx
+++ theme.tsx
@@ -1,14 +1,21 @@
 import ConfigProvider from "antd/es/config-provider"
 import type { ReactNode } from "react"
+import { useEffect } from "react"
 
-export const ThemeProvider = ({ children = null as ReactNode }) => (
-  <ConfigProvider
-    theme={{
-      token: {
-        colorPrimary: "hotpink",
-        colorLink: "hotpink"
-      }
-    }}>
-    {children}
-  </ConfigProvider>
-)
+export const ThemeProvider = ({ children = null as ReactNode }) => {
+  useEffect(() => {
+    //将根元素的字体大小设置为16px
+    document.documentElement.style.fontSize = "16px"
+  }, [])
+  return (
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: "hotpink",
+          colorLink: "hotpink"
+        }
+      }}>
+      {children}
+    </ConfigProvider>
+  )
+}
